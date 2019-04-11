b45074a05bd6779233ee270ba53beecaa0aee476
1164676 <1164676>
2019-04-11 17:06:55 -0300

    Enviando Lista 16

---
+++ Lista16/Ex01/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex01
+{
+    interface IMedia
+    {
+        int GetMedia();
+    }
+    class DisciplinaSemestral : IMedia
+    {
+        private int nota1, nota2, notaF;
+        public DisciplinaSemestral(int n1, int n2, int nf)
+        {
+            nota1 = n1;
+            nota2 = n2;
+            notaF = nf;
+        }
+        public int GetMedia()
+        {
+            int m = (2 * nota1 + 3 * nota2) / 5;
+            if (m < 60) m = (m + notaF) / 2;
+            return m;
+        }
+    }
+    class DisciplinaAnual : IMedia
+    {
+        private int nota1, nota2, nota3, nota4, notaF;
+        public DisciplinaAnual(int n1, int n2, int n3, int n4, int nf)
+        {
+            nota1 = n1;
+            nota2 = n2;
+            nota3 = n3;
+            nota4 = n4;
+            notaF = nf;
+        }
+        public int GetMedia()
+        {
+            int m = (2 * nota1 + 2 * nota2 + 3 * nota3 + 3 * nota4) / 10;
+            if (m < 60) m = (m + notaF) / 2;
+            return m;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IMedia[] v = new IMedia[4];
+            v[0] = new DisciplinaSemestral(100, 50, 0);
+            v[1] = new DisciplinaSemestral(80, 60, 0);
+            v[2] = new DisciplinaAnual(100, 75, 40, 0, 30);
+            v[3] = new DisciplinaAnual(100, 50, 50, 60, 0);
+            for (int i = 0; i < 4; i++)
+                Console.WriteLine(v[i].GetMedia());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lista16/Ex04/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex04
+{
+    class Pais : IComparable
+    {
+        private string nome;
+        private int ouro, prata, bronze;
+        public Pais(string n, int o, int p, int b)
+        {
+            nome = n;
+            ouro = o;
+            prata = p;
+            bronze = b;
+        }
+        public int CompareTo(object obj)
+        {
+            Pais x = obj as Pais;
+            if (this.ouro > x.ouro) return -1;
+            if (this.ouro < x.ouro) return 1;
+            if (this.prata > x.prata) return -1;
+            if (this.prata < x.prata) return 1;
+            if (this.bronze > x.bronze) return -1;
+            if (this.bronze < x.bronze) return 1;
+            return this.nome.CompareTo(x.nome);
+        }
+        public override string ToString()
+        {
+            return $"{nome} {ouro} {prata} {bronze}";
+        }
+    }
+    class Teste
+    {
+        public static void Ordenar(IComparable[] p)
+        {
+            for (int i = 0; i <= p.Length - 2; i++)
+                for (int j = i + 1; j <= p.Length - 1; j++)
+                    if (p[i].CompareTo(p[j]) == 1)
+                    {
+                        IComparable aux = p[i];
+                        p[i] = p[j];
+                        p[j] = aux;
+                    }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pais p1 = new Pais("Brasil", 10, 5, 3);
+            Pais p2 = new Pais("Argentina", 10, 5, 3);
+            Pais p3 = new Pais("Japão", 18, 9, 4);
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+            Console.WriteLine(p3);
+            Pais[] x = new Pais[3];
+            x[0] = p1;
+            x[1] = p2;
+            x[2] = p3;
+            //Array.Sort(x);
+            Teste.Ordenar(x);
+            Console.WriteLine(x[0]);
+            Console.WriteLine(x[1]);
+            Console.WriteLine(x[2]);
+            Console.ReadKey();
+        }
+    }
+}
